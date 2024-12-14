80a5d25335ee833d3ab177a50eb56ed117da57f6
Sumanth0938 <sumanth.yaddanapudi@maangtechnologies.com>
2024-12-14 18:18:35 +0530

    updated database

--- src/const.py
+++ src/const.py
@@ -1,14 +1,14 @@
 import os
 from urllib.parse import quote
 
-POSTGRES_DB_USERNAME = 'workshop-aws-user'
-POSTGRES_DB_PSWD = 'workshop-aws-user!?!?'
+POSTGRES_DB_USERNAME = ''
+POSTGRES_DB_PSWD = ''
 encoded_password = quote(POSTGRES_DB_PSWD, safe='')
-POSTGRES_DB_URL = '178.16.139.18:5432'
-POSTGRES_DB_NAME = 'workshop-aws'
+POSTGRES_DB_URL = ''
+POSTGRES_DB_NAME = ''
 
-# POSTGRES_DATABASE_URL = 'sqlite:///./spp.db'
-POSTGRES_DATABASE_URL = f'postgresql://{POSTGRES_DB_USERNAME}:{encoded_password}@{POSTGRES_DB_URL}/{POSTGRES_DB_NAME}'
+POSTGRES_DATABASE_URL = 'sqlite:///./aws.db'
+# POSTGRES_DATABASE_URL = f'postgresql://{POSTGRES_DB_USERNAME}:{encoded_password}@{POSTGRES_DB_URL}/{POSTGRES_DB_NAME}'
 
 PREFIX_URL = "/contact"
 USER_NAME = "FangLing"
